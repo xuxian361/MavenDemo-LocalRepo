4e29d282dd187eac626b4b50fd512b973d51fe58
xuxian361 <308379285@qq.com>
2017-03-07 09:37:56 +0800

    添加一个Library
    添加一个Library, 这个Library 是使用Nexus 搭建Maven 私服的Library.

---
+++ ModuleFounder/nexuslib/src/main/java/com/dragonpass/sundy/nexuslib/CommonUtils.java
@@ -0,0 +1,18 @@
+package com.dragonpass.sundy.nexuslib;
+
+import android.util.Log;
+
+/**
+ * Created by sundy on 17/3/6.
+ */
+
+public class CommonUtils {
+
+    private static final String TAG = "CommonUtils";
+
+    public static void doSomeThing() {
+        Log.e(TAG, "------->doSomeThing");
+    }
+
+
+}
